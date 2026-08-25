0398d7d9fe164917ae09ef0f973115aeb42e6e0e
NicoB <niko44@gmail.com>
2026-08-25 15:18:51 +0200

    Add SECRET_KEY environment variable to Docker run

--- docker-script.sh
+++ docker-script.sh
@@ -3,4 +3,4 @@
 docker stop suivi-subvention-cse
 docker rm suivi-subvention-cse
 docker build -t suivi-subvention-cse .
-docker run -d -p 5100:5100 --name suivi-subvention-cse  -v /volume3/docker/cse/upload:/app/upload -v /volume3/docker/cse/bdd:/app/bdd suivi-subvention-cse
+docker run -d -p 5100:5100 --name suivi-subvention-cse -e SECRET_KEY=ACHANGER -v /volume3/docker/cse/upload:/app/upload -v /volume3/docker/cse/bdd:/app/bdd suivi-subvention-cse
